c9d7393a5a291bafd03d5b04e40b65053a7ce308
pypkaed <89941898+pypkaed@users.noreply.github.com>
2022-05-07 00:43:37 +0300

    Update basic_algo.cpp

--- lab6/basic_algo.cpp
+++ lab6/basic_algo.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <array>
-#include <list>
-#include <set>
 #include <string>
 #include "algos.h"
+#include "CPoint.h"
 // 6 варик: all_of, is_partitioned, find_backward
+
+CPoint compare_point(2, 4);
 
 template<typename T>
 bool predicate(T element) {
-    return element <= 8;
+    return element <= compare_point;
 }
 
 int main() {
     // === VECTOR OF INTEGERS ===
-    std::vector<int> a = {9, 8, 7, 6};
-    std::cout << "--- vector check ---\n";
-    std::cout << all_of(a.begin(), a.end(), predicate<int>);
-    std::cout << "\n";
-    std::cout << is_partitioned(a.begin(), a.end(), predicate<int>);
-    std::cout << "\n";
-    std::cout << a[a.size() - find_backward(a.end(), a.begin(), 6)];
+//    std::vector<int> a = {9, 8, 7, 6};
+//    std::cout << "--- vector check ---\n";
+//    std::cout << all_of(a.begin(), a.end(), predicate<int>);
+//    std::cout << "\n";
+//    std::cout << is_partitioned(a.begin(), a.end(), predicate<int>);
+//    std::cout << "\n";
+//    std::cout << a[a.size() - find_backward(a.end(), a.begin(), 6)];
 
-    // === ARRAY OF STRINGS ===
+//     === ARRAY OF STRINGS ===
 //    std::array<std::string, 4> b = {"a", "s", "ass", "s"};
 //    std::cout << "--- array check ---\n";
 //    std::cout << all_of(b.begin(), b.end(), predicate<std::string>);
@@ -31,6 +33,17 @@ int main() {
 //    std::cout << "\n";
 //    std::cout << b[b.size() - find_backward(b.end(), b.begin(), "ass")];
 
+    // === VECTOR OF CPoints ===
+    std::vector<CPoint> c;
+    CPoint point1(1, 2);
+    CPoint point2(2, 4);
+    c.push_back(point1); c.push_back(point2);
+    std::cout << "--- vector of user class check ---\n";
+    std::cout << all_of(c.begin(), c.end(), predicate<CPoint>);
+    std::cout << "\n";
+    std::cout << is_partitioned(c.begin(), c.end(), predicate<CPoint>);
+    std::cout << "\n";
+    std::cout << c[c.size() - find_backward(c.end(), c.begin(), compare_point)];
 
     return 0;
 }
